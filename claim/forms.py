457aba0a7d09ff566f9ea264ad046164aff3d188
Haziq Aiman <haziqaimanfb@gmail.com>
2024-06-27 10:09:38 +0800

    Fixed claim submission

--- claim/forms.py
+++ claim/forms.py
@@ -8,12 +8,19 @@ class ClaimForm(forms.ModelForm):
     program_end_date = forms.DateTimeField(
         widget=forms.DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control'})
     )
-    program_letter_file = forms.FileField(
-        widget=forms.FileInput(attrs={'type': 'file', 'class': 'form-control'})
-    )
+    
     class Meta:
         model = Claim
         fields = ["program_name", "program_start_date", "program_end_date","program_mode", "claim_month", "claim_year", "program_location", "program_letter_file","accommodation_choice", "accommodation_amount", "accommodation_file", "travel_amount", "travel_file","meal_choice","meal_file", "toll_amount","toll_file", "parking_amount","parking_file", "plane_ticket_amount","plane_ticket_file", "other_amount","other_file"]
+        widgets = {
+            "program_letter_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+            "accommodation_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+            "travel_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+            "toll_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+            "parking_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+            "plane_ticket_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+            "other_file": forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}),
+        }
 
 class ApproveForm(forms.ModelForm):
     class Meta:
