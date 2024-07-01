0b8ef4fceff08be974c27dfc5c6735a16a0d6dc6
Haziq Aiman <haziqaimanfb@gmail.com>
2024-07-01 11:16:30 +0800

    Added new template tags, has_group

--- helper/templatetags/custom_filters.py
+++ helper/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def get(dictionary, key):
     return dictionary.get(key)
+
+@register.filter(name='has_group') 
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists() 
