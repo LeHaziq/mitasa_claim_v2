41526270c98a75d8ab71e3c89e2a223a9507cc45
Haziq Aiman <haziqaimanfb@gmail.com>
2024-07-01 13:28:25 +0800

    Registered medical models to admin site

--- medical_claim/admin.py
+++ medical_claim/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Medical_Claim, Approved_Medical_Claim, Rejected_Medical_Claim
 
-# Register your models here.
+admin.site.register(Medical_Claim)
+admin.site.register(Approved_Medical_Claim)
+admin.site.register(Rejected_Medical_Claim)
